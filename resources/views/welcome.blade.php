5c33cfa73d501a030b01cb3a2142e18d2da7e6cd
Ebbe Wertz <ebbe.wertz8@gmail.com>
2025-07-23 13:35:11 +0200

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>JongNL Neeritter</title>
+  <link rel="icon" type="image/x-icon" href="favicon.svg">
+  @vite('resources/js/app.js')
+  @vite('resources/css/app.css')
+</head>
+<body>
+    <div id="app">
+        {{-- <h1>Jong Nederland Neeritter</h1>
+  <p>
+    Op dit domein komt de wesite voor Jong Nederland Neeritter.
+    <br>
+    Hier wordt momenteel offline aan gewerkt. Wanneer de site af is,
+    <br>
+    wordt deze statische HTML pagina vervangen door de nieuwe site.
+</p> --}}
+        <counter />
+    </div>
+
+
+</body>
+</html>
