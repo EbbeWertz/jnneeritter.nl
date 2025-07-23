832bd97a4af12cb23eedaac93692cb274b509f1d
Ebbe Wertz <ebbe.wertz8@gmail.com>
2025-07-24 00:06:42 +0200

    dynamic load icon system gemaakt

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,22 @@
 import './bootstrap';
 
-import { createApp } from 'vue'
-import Counter from './components/Counter.vue'
+import { createApp, defineAsyncComponent } from 'vue'
+import Knop from './components/Knop.vue'
+import NavbarLinkKnop from './components/NavbarLinkKnop.vue'
 
 
 const app = createApp()
-app.component('counter', Counter)
 
+
+const icons = import.meta.glob('/resources/raw_icons/*.svg')
+for (const path in icons) {
+    const name = 'i-' + path.slice(path.lastIndexOf('/') + 1).replace('.svg', '')
+    app.component(name, defineAsyncComponent(() => icons[path]()))
+}
+
+
+
+app.component('Knop', Knop)
+app.component('Navlink', NavbarLinkKnop)
 
 app.mount('#app')
